a2e365ddc0f7fa85dbb61e6b28be797933032b2d
bookthief666 <manuel.orrantia@gmail.com>
2026-08-10 02:32:29 -0700

    chore: remove unused Sanctum query profile

--- src/scene/sanctumProfile.ts
+++
@@ -1,5 +0,0 @@
-export function legacySanctumEnabled(
-  search = typeof window === 'undefined' ? '' : window.location.search,
-) {
-  return new URLSearchParams(search).get('legacySanctum') === '1'
-}
